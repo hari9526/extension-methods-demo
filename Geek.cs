b4912056f7652aa91ecc75f4b12d9f8858694c6e
Harikrishnan R <hari04.krishnan@gmail.com>
2022-02-22 16:25:35 +0530

    Initial set up

---
+++ Geek.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public class Geek
+    {
+        // Method 1
+        public void M1()
+        {
+            Console.WriteLine("Method Name: M1");
+        }
+
+        // Method 2
+        public void M2()
+        {
+            Console.WriteLine("Method Name: M2");
+        }
+
+        // Method 3
+        public void M3()
+        {
+            Console.WriteLine("Method Name: M3");
+        }
+    }
+}
