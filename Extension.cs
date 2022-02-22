31decd8efe68e8c39b967fdf34aaea5e8b7570f5
Harikrishnan R <hari04.krishnan@gmail.com>
2022-02-22 19:41:22 +0530

    Implement extension

---
+++ Extension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public static class Extension
+    {
+        public static void M4(this Geek g)
+        {
+            Console.WriteLine("Method Name: M4"); 
+        }
+        
+        public static void M5(this Geek g, string msg)
+        {
+            Console.WriteLine(msg); 
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,12 @@ namespace ExtensionMethods
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Geek g = new Geek();
+            g.M1();
+            g.M2();
+            g.M3();
+            g.M4();
+            g.M5("Method Name: M5");
         }
     }
 }
